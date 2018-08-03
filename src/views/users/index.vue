daa2f60c4b3eac369665a3d58d89aacf02132d59
mvalladares <mvalladares@chileautos.cl>
2018-08-03 12:14:21 -0400

    Delte

--- src/views/users/index.vue
+++ src/views/users/index.vue
@@ -5,7 +5,7 @@
     <hr>
     <ul>
         <li v-for="user in users" :key="user._id">
-         {{ user.first_name }} | <button>Editar</button> | <button>Borrar</button>
+         {{ user.first_name }} | <button>Editar</button> | <button @click="deleteUser(user._id)" >Borrar</button>
         </li>
     </ul>
 </div>
@@ -17,17 +17,24 @@ let url = "https://dry-cove-41154.herokuapp.com/";
 import axios from "axios";
 
 export default {
-  data: {
-    return: {
-      users: {}
+  data() {
+    return {
+      users: {},
+      data:''
     }
   },
   methods: {
     getUsers() {
       axios.get(url + "users").then(response => {
-        this.users = response.data;
-        console.log(this.users)
+        this.users = response.data
+        //console.log(this.users);
       });
+    },
+    deleteUser(id){
+        axios.delete(url + 'users/'+ id)
+        .then(response=>{
+            this.getUsers();
+        })
     }
   },
   created(){
