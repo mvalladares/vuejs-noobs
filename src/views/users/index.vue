3297a38f253ba7592f10756848bbd0e55cf87477
mvalladares <mvalladares@chileautos.cl>
2018-08-03 11:50:34 -0400

    Maquetacion fea

---
+++ src/views/users/index.vue
@@ -0,0 +1,13 @@
+<template>
+<div>
+    <h1>Users</h1>
+    <button>Nuevo</button>
+    <hr>
+    <ul>
+        <li>
+            Alejandro | <button>Editar</button> | <button>Borrar</button>
+        </li>
+    </ul>
+</div>
+    
+</template>
